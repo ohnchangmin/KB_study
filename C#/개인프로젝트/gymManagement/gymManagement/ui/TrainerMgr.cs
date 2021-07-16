04343716933850659d2386c924e94324bf4430ac
ohnchangmin <ok_ddol@naver.com>
2021-07-16 16:49:28 +0900

    init textbox

--- C#/개인프로젝트/gymManagement/gymManagement/ui/TrainerMgr.cs
+++ C#/개인프로젝트/gymManagement/gymManagement/ui/TrainerMgr.cs
@@ -23,6 +23,8 @@ namespace gymManagement.ui
 
         private void TrainerMgr_Load(object sender, EventArgs e)
         {
+            TextBox[] arr = {trainerName, trainerRrn1, trainerRrn2, trainerTel1, trainerTel2, trainerTel3,
+                            trainerAddress, trainerJobType, trainerRank, trainerSalary};
             showList(ad.getTrainerVo());
         }
         private void showList(List<Trainer> list)
@@ -44,20 +46,32 @@ namespace gymManagement.ui
             string salary = trainerSalary.Text;
 
             List<Trainer> list = new List<Trainer>();
+           
             list.Add(new Trainer(name, rrn, tel, address, jobType, rank, salary));
             return list;
         }
 
+        private void insertFormClear(TextBox[] arr)
+        {
+            /*TextBox[] arr = {trainerName, trainerRrn1, trainerRrn2, trainerTel1, trainerTel2, trainerTel3,
+                            trainerAddress, trainerJobType, trainerRank, trainerSalary};*/
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i].Clear();
+            }
+        }
+
+       /* private void initTextBox()
+        {
+            TextBox[] arr = {trainerName, trainerRrn1, trainerRrn2, trainerTel1, trainerTel2, trainerTel3,
+                            trainerAddress, trainerJobType, trainerRank, trainerSalary};
+        }*/
+
         private void trainerInsert_Click(object sender, EventArgs e)
         {
             showList(insertTrainer());
-            trainerName.Clear();
-            trainerRrn1.Clear();
-            trainerRrn2.Clear();
-            trainerTel1.Clear();
-            trainerTel2.Clear();
-            trainerTel3.Clear();
-            trainerAddress.Clear();
+            insertFormClear();
         }
     }
 }
